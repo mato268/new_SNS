8ca8c9777a4ff3349d9445f0f361cf96a2518810
Sangjun Lee <mato268@naver.com>
2024-01-15 21:13:45 +0900

    feat:App.tsx 수정

--- newsns/src/App.tsx
+++ newsns/src/App.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import { Input } from "./component/Input";
 
 function App() {
-  return (<></>
+  return (
+    <main>
+      {/*인풋 구분하기 위함*/}
+      <div style={{ backgroundColor: "blue", width: "390px", height: "200px" }}>
+        <Input
+          type="text"
+          size="large"
+          color="large"
+          placeholder="닉네임을 입력해주세요"
+        ></Input>
+        <Input
+          type="text"
+          size="medium"
+          color="medium"
+          placeholder="댓글을 입력해주세요"
+        ></Input>
+        <Input
+          type="text"
+          size="outline"
+          color="outline"
+          placeholder="제목을 입력해주세요"
+        ></Input>
+      </div>
+    </main>
   );
 }
 
