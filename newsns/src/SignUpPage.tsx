8ebedb852fc94a0c7c22f7ca061099f4f14190fe
Sangjun Lee <mato268@naver.com>
2024-02-02 21:36:18 +0900

    feat : height 수정, 닉네임 설명 구현

--- newsns/src/SignUpPage.tsx
+++ newsns/src/SignUpPage.tsx
@@ -1,33 +1,68 @@
 import { Button } from "./components/Button";
 import { Input } from "./components/Input";
 import Typo from "./components/Typo";
+import { useState } from "react";
+import { ChangeEvent } from "react";
 
 export default function SignUpPage() {
+  const [nickname, setNickname] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleNicknameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setNickname(value);
+
+    if (value.length > 6) {
+      setErrorMessage("/*6글자 이상입니다*/");
+    } else {
+      setErrorMessage("");
+    }
+  };
   return (
-    <div className="bg-deepdark w-width sm:w-width md:w-xWidth lg:w-xLwidth h-height sm:h-height md:h-xheight lg:h-xLheight flex justify-center text-center items-center relative">
-      <div className="absolute top-1/3 h-full -mt-48 space-y-8">
-        <Typo tag="h1" fonts="korea" sizes="xLarge" weights="normal" fontColor="yellow">
+    <div className="bg-deepdark w-width sm:w-width md:w-xWidth lg:w-xLwidth h-full flex justify-center text-center items-center relative">
+      <div className="absolute top-1/3 -mt-48 space-y-8">
+        <Typo
+          tag="h1"
+          fonts="korea"
+          sizes="xLarge"
+          weights="normal"
+          fontColor="yellow"
+        >
           /*회원가입을 환영해*/
         </Typo>
-        <div className="space-y-8">
-            <Input
-              type="text"
-              sizes="large"
-              colors="white"
-              rightNode="iconName1"
-              placeholder="닉네임을 입력해주세요"
-              variant="outline"
-            ></Input>
+        <div className="space-y-2">
+          <div className="relative">
+            {nickname.length === 0 && (
+              <Typo tag="p" fonts="korea" sizes="small" fontColor="green">
+                /*6글자까지 가능합니다*/
+              </Typo>
+            )}
+            {errorMessage && (
+              <Typo tag="p" fonts="korea" sizes="small" fontColor="red">
+                {errorMessage}
+              </Typo>
+            )}
+          </div>
+          <Input
+            type="text"
+            sizes="large"
+            colors="white"
+            rightNode="iconName1"
+            placeholder="닉네임을 입력해주세요"
+            value={nickname}
+            onChange={handleNicknameChange}
+            variant="outline"
+          ></Input>
         </div>
         <div className="space-y-8">
-            <Input
-              type="text"
-              sizes="large"
-              colors="white"
-              rightNode="iconName2"
-              placeholder="이메일을 입력해 주세요"
-              variant="outline"
-            ></Input>
+          <Input
+            type="text"
+            sizes="large"
+            colors="white"
+            rightNode="iconName2"
+            placeholder="이메일을 입력해 주세요"
+            variant="outline"
+          ></Input>
         </div>
         <div className="absolute right-8">
           <Button sizes="Confirmed" colors="yellow">
